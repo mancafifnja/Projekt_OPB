ddc60fe02f62aa87691f2d858475c123dc7c20e1
JurijTekavecDevelopment <Jurij.Tekavec.Development@gmail.vom>
2021-09-08 14:27:47 +0000

    Name change and bugfix

--- app/backend/onGetContent.js
+++ app/backend/onGetContent.js
@@ -10,15 +10,15 @@ export default async function (req, res) {
 	try {
 		console.log("Request on get contents: " + JSON.stringify(req.body))
 
-		var query = "SELECT id_živali,oglas,pasma,kategorija,spol,cena,starost,število_nog,opis,slika,živali.datum,prodajalec,id_uporabnik,uporabniško_ime,telefon FROM živali JOIN uporabniki ON živali.prodajalec = uporabniki.id_uporabnik ORDER BY id_živali DESC LIMIT 10"
+		var query = "SELECT id_živali,oglas,pasma,kategorija,spol,cena,starost,število_nog,opis,slika,živali.datum,prodajalec,id_uporabnik,uporabniško_ime,telefon,živali.kraj FROM živali JOIN uporabniki ON živali.prodajalec = uporabniki.id_uporabnik ORDER BY id_živali DESC LIMIT 10"
 		var needsAND = false;
 
 		if( req.body.place || req.body.category || req.body.subCategory ){
-			query = "SELECT id_živali,oglas,pasma,kategorija,spol,cena,starost,število_nog,opis,slika,živali.datum,prodajalec,id_uporabnik,uporabniško_ime,telefon FROM živali JOIN uporabniki ON živali.prodajalec = uporabniki.id_uporabnik WHERE ";
+			query = "SELECT id_živali,oglas,pasma,kategorija,spol,cena,starost,število_nog,opis,slika,živali.datum,prodajalec,id_uporabnik,uporabniško_ime,telefon,živali.kraj FROM živali JOIN uporabniki ON živali.prodajalec = uporabniki.id_uporabnik WHERE ";
 		}
 		if( req.body.place  ){
 
-			query += " kraj = '" + req.body.place + "'";
+			query += " živali.kraj = '" + req.body.place + "'";
 			needsAND = true;
 		}
 		
